a2978ccf32616807566f1b0978a76798c2ade3ad
Brayan Estrada Posada <bestradap.qintess@bancochile.cl>
2025-03-14 18:47:40 -0500

    first commit

---
+++ src/test/java/com/brbank/ms_credit_card/MsCreditCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brbank.ms_credit_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
